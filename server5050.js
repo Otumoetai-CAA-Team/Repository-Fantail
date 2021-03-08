8464542e1ba02c35b62d0a8f48b1ecde67e40515
Chase <chasewinder912@gmail.com>
2021-03-08 20:45:26 +1300

    Hover effect for buttons

--- server5050.js
+++ server5050.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '/')));
 const connenction = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'tnetennba123',
+    password: 'thermochemistry2021',
     database: 'mydb',
     charset: 'utf8'
 });
