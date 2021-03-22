c5468338d448979dd873605e4d980e9778efe73d
AlexFreeman1449 <65686491+AlexFreeman1449@users.noreply.github.com>
2021-03-23 12:11:22 +1300

    Change file directories

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ connenction.query($query, function(err, rows){
 
 app.get('/education', function(req, res){
 
-    fs.readFile('education.html', (err, data_education) =>{
+    fs.readFile('./HTML/education.html', (err, data_education) =>{
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
         res.write(data_education, 'utf8')
         res.end();
@@ -74,7 +74,7 @@ app.get('/education', function(req, res){
 
 app.get('/writing', function(req, res){
 
-    fs.readFile('writing.html', (err, data_writing) =>{
+    fs.readFile('./HTML/writing.html', (err, data_writing) =>{
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
         res.write(data_writing, 'utf8')
         res.end();
@@ -85,7 +85,7 @@ app.get('/writing', function(req, res){
 
 
 app.get('/meditation', function(req, res){
-    fs.readFile('meditation.html', (err, data_med) =>{
+    fs.readFile('./HTML/meditation.html', (err, data_med) =>{
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
         res.write(data_med, 'utf8')
         res.end()
@@ -93,7 +93,7 @@ app.get('/meditation', function(req, res){
 })
 
 app.get('/about', function(req, res){
-    fs.readFile('about.html', (err, data_about) =>{
+    fs.readFile('./HTML/about.html', (err, data_about) =>{
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
         res.write(data_about, 'utf8')
         res.end()
@@ -101,7 +101,7 @@ app.get('/about', function(req, res){
 })
 
 app.get('/contact', function(req, res){
-    fs.readFile('contact.html', (err, data_contact) =>{
+    fs.readFile('./HTML/contact.html', (err, data_contact) =>{
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
         res.write(data_contact, 'utf8')
         res.end()
