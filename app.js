7ba08b54d5e7dbe57ad75bf5901aaf4c88a7ddf6
AlexFreeman1449 <65686491+AlexFreeman1449@users.noreply.github.com>
2021-03-26 13:42:46 +1300

    change main js file exclude mysql

--- app.js
+++ app.js
@@ -19,46 +19,8 @@ const connenction = mysql.createConnection({
 });
 
 app.get('/', function(req, res){
+    res.render('home_page')
 
- connenction.connect(function(err){
-    if(err){
-        console.log("Error connecting to the database")
-    }
-
-$query = 'SELECT * FROM mydb.book';
-connenction.query($query, function(err, rows){
-
-    if(err){
-        console.log("Error in executing the query");
-    }
-
-    var table_dataNames = []
-    for(var i =0; i < 3; i ++){
-        table_dataNames.push(rows[i].author)
-    }
-
-    var table_dataJobs = []
-    for(var i =0; i < 3; i ++){
-        table_dataJobs.push(rows[i].title)
-    } 
-    var data = {name: table_dataNames, 
-        title: table_dataJobs}
-    
- 
-    res.render('home_page', {data,data})
-    // fs.readFile('home_page.html', (err, data) =>{
-    //     res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-    //     res.write(data, 'utf8')
-    //     res.write('In our team we have: <br>');
-    //     for(var i =0; i < 3; i ++){
-    //         res.write(rows[i].author + ', ')
-    //         res.write('<body>who is: ' + rows[i].title + '<br> </body>')
-        
-    //     }
-    //     res.end();
-    // })
-})
-})
 });
 
 
