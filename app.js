700c7b077bec0fb04180365d676d66a6f78cd904
AlexFreeman1449 <65686491+AlexFreeman1449@users.noreply.github.com>
2021-05-24 14:39:03 +1200

    deleting comments/trial req res

--- app.js
+++ app.js
@@ -37,9 +37,6 @@ MongoClient.connect(uri, { useUnifiedTopology: true }, function(err, db) {
     });
 });
 
-// app.get('/', function(req, res){
-//     res.render('home_page')
-// });
 
 app.get('/education', function(req, res){
 
