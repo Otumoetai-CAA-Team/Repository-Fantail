7c1c65beae3c10fa398fd6c99eb4b824138b849e
Your Name <mcaulaya@stu.otc.school.nz>
2021-06-14 14:36:30 +1200

    moved index.ejs into views folder

--- upload.js
+++ upload.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const crypto = require('crypto');
+const mongoose = require('mongoose')
+const GridFsStorage = require('multer-gridfs-storage')
+const Grid = require('gridfs-stream')
+const methodOveride = require('method-overide')
+
 const upload = express();
+
 upload.set('view engine', 'ejs');
 
 upload.get('/', (req, res) => {
@@ -8,7 +17,6 @@ upload.get('/', (req, res) => {
 
 const port = 5000;
 
-// ${} not working maybe?
 upload.listen(port, () => console.log('Server started on port ${port}'));
 
 
