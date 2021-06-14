afc51ade11500e8d9ecb7bfe5d8ac285b113ebc3
Your Name <mcaulaya@stu.otc.school.nz>
2021-06-14 14:54:57 +1200

    add middleware, mongo conn, init gfs and routing

--- upload.js
+++ upload.js
@@ -9,10 +9,58 @@ const methodOveride = require('method-overide')
 
 const upload = express();
 
+// Middleware
+upload.use(bodyParser.json());
+upload.use(methodOveride('_method'));
 upload.set('view engine', 'ejs');
 
+// Mongo URI
+const mongoURI= 'mongodb+srv://fyeard1449:hcGBE6g5i7ZhuodU@clusterm.zscdl.mongodb.net/amy';
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+    //Init stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+})
+
+// Create storage engine
+const storage = new GridFsStorage({
+    url: config.mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+
+const upload = multer({ storage });
+
+// @route GET /
+// @desc Loads form
 upload.get('/', (req, res) => {
     res.render('index');
+});
+
+// @route POST /
+upload.post('upload', upload.single('file'), (req, res) => {
+    res.json({file: req.file});
+
 });
 
 const port = 5000;
